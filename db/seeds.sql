35ef58b2bed71f8292abc5c73bc4d7e8ad3c087f
Zaldana <alexandrazaldana@gmail.com>
2019-03-31 15:40:53 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("New Bacon-Ings", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Foot Feta-Ish Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Rest in Peas Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Thank God its Fried Egg Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krabby Patty", false);
